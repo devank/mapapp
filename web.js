29b464c4fbfc49cfc4fba64009b4dc19aba76fd7
Devan K <devan.nitc@gmail.com>
2014-02-13 05:57:32 +0000

    added web.js

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var fs = require('fs');
+  var buf = new Buffer(256);
+  buf = fs.readFileSync('index.html');
+  response.send(buf.toString('utf8',0,buf.length));
+});
+
+var port = process.env.PORT || 8000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
